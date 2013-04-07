3b9d0f520db36a40346cefccfd31816154c8ee16
AC <antiherochen@antiherochen-Rev-1-0.(none)>
2013-04-07 13:37:52 +0800

    An new max-match.py

--- 2-python-practice/3-max-matching-word-segmentation/max-match.py
+++ 2-python-practice/3-max-matching-word-segmentation/max-match.py
@@ -4,16 +4,22 @@ import sys
 
 def max_match_segment( line, dic ):
     # write your code here
+    line=unicode(line,"utf-8")
     ret=[]
-    last=len(line)-1 # There is a enter notation in the end of the sentence.
-    while last>0:
-        for i in range(0,last):
-            if line[i:last] in dic:
-                ret.append(line[i:last])
-                last=i
+    beg=0 
+    #print line
+    #print line[0],len(line)
+    while beg<len(line):
+        for i in range(len(line),beg,-1):
+            key=line[beg:i].encode("utf-8")
+            if key in dic:
+                ret.append(key)
+                beg=i
                 break
         else:
-            return False
+            key=line[beg:beg+1].encode("utf-8")
+            ret.append(key)
+            beg+=1
     return ret
 
 
@@ -26,11 +32,20 @@ if __name__=="__main__":
         sys.exit(1)
 
     try:
-        dic = pickle.load(sys.argv[2])
+        fdic=open(sys.argv[2],"r")
+        dic = pickle.load(fdic)
     except:
         print >> sys.stderr, "failed to load dict"
         sys.exit(1)
 
+    #for key in dic:
+    #    print key
     for line in fpi:
-        print "\t".join( max_match_segment(line.strip(), dic) )
+        #line.decode("utf-8")
+        #print "\t".join(line.strip().split() )
+        #for word in line:
+        #    print word,
+        #print
+        #print line.decode("utf-8")
+        print  "\t".join(max_match_segment(line.strip(), dic)) 
 
